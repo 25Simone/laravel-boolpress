7c144ba4a5c8e0674f04e2ef052ef81d94a09019
25Simone <dantsimone111@gmail.com>
2022-03-15 14:49:55 +0100

    add register option in vue navbar

--- public/js/vue.js
+++ public/js/vue.js
@@ -119,6 +119,9 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({});
 
 /***/ }),
@@ -680,6 +683,14 @@ var staticRenderFns = [
                     "a",
                     { staticClass: "nav-link", attrs: { href: "/login" } },
                     [_vm._v(" Login ")]
+                  ),
+                ]),
+                _vm._v(" "),
+                _c("li", { staticClass: "nav-item" }, [
+                  _c(
+                    "a",
+                    { staticClass: "nav-link", attrs: { href: "/register" } },
+                    [_vm._v(" Register ")]
                   ),
                 ]),
               ]),
--- resources/js/components/TheNavbar.vue
+++ resources/js/components/TheNavbar.vue
@@ -15,7 +15,10 @@
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="/login"> Login </a>
-                     </li>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/register"> Register </a>
+                    </li>
                 </ul>
             </div>
         </div>
